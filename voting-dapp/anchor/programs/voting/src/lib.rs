45bbf0290030c02216fd325104281bc795d4816c
luja <lujachitrakar156@gmail.com>
2025-04-03 17:06:50 +0000

    starting project

---
+++ voting-dapp/anchor/programs/voting/src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(clippy::result_large_err)]
+
+use anchor_lang::prelude::*;
+
+declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+#[program]
+pub mod voting{
+
+
+    use super::*;
+
+    // pub fn initialize_poll(_ctx:Context<InitializePoll>,_poll_id:u64)->Result<()>{
+    //     Ok(())
+    // }
+}
