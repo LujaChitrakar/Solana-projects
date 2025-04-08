0872e5f56a6686b6f2b681b97fc2c8e463b16eb2
luja <lujachitrakar156@gmail.com>
2025-04-08 08:35:28 +0000

    initializing project

---
+++ swap/programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("856mRRm3jcqe4cR4EvqP2HkuKWdC933wUMW5DqTwXRqY");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
