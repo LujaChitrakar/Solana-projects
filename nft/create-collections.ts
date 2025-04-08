f732c02c26aad7bb25e2e75bb133779e596afff2
luja <lujachitrakar156@gmail.com>
2025-04-08 05:44:32 +0000

    initializing new project

---
+++ nft/create-collections.ts
@@ -0,0 +1,15 @@
+import {
+  createNft,
+  fetchDigitalAsset,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+
+import {
+  airdropIfRequired,
+  getExplorerLink,
+  getKeypairFromFile,
+} from "@solana-developers/helpers";
+
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+
+import { Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
