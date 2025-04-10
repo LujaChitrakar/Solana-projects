b27118bfe5202e939ab6e04e0894c0a6087263c1
luja <lujachitrakar156@gmail.com>
2025-04-10 12:11:03 +0000

    completing initialize lottery

--- token-lottery/anchor/programs/tokenlottery/src/instructions/instructions.rs
+++ token-lottery/anchor/programs/tokenlottery/src/instructions/instructions.rs
@@ -1,9 +1,15 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::Metadata,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
 
+/*----------- INITIALIZE CONFIG -----------*/
 #[derive(Accounts)]
-pub struct InitializeConfig<'info>{
+pub struct InitializeConfig<'info> {
     #[account(mut)]
-    pub payer:Signer<'info>,
+    pub payer: Signer<'info>,
 
     #[account(
         init,
@@ -12,22 +18,80 @@ pub struct InitializeConfig<'info>{
         seeds=[b"token_lottery"],
         bump
     )]
-    pub token_lottery:Account<'info,TokenLottery>,
+    pub token_lottery: Account<'info, TokenLottery>,
 
-    pub system_program:Program<'info,System>
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct TokenLottery{
-    pub winner:u64,
-    pub winner_chosen:bool,
-    pub start_time:u64,
-    pub end_time:u64,
-    pub lottery_pot_amount:u64,
-    pub total_tickets:u64,
-    pub ticket_price:u64,
-    pub authority:Pubkey,
-    pub randomness_account:Pubkey,
-    pub bump:u8,
+pub struct TokenLottery {
+    pub winner: u64,
+    pub winner_chosen: bool,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub lottery_pot_amount: u64,
+    pub total_tickets: u64,
+    pub ticket_price: u64,
+    pub authority: Pubkey,
+    pub randomness_account: Pubkey,
+    pub bump: u8,
+}
+
+/*----------- INITIALIZE CONFIG -----------*/
+
+#[derive(Accounts)]
+pub struct InitializeLottery<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer=payer,
+        mint::decimals=0,
+        mint::authority=collection_mint,
+        mint::freeze_authority=collection_mint,
+        seeds=[b"collection_mint".as_ref()],
+        bump
+    )]
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        payer=payer,
+        token::mint=collection_mint,
+        token::authority=collection_token_account,
+        seeds=[b"collection_associated_token".as_ref()],
+        bump
+    )]
+    pub collection_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    /// CHECK: This account is validated by the token metadata program during instruction execution.
+    #[account(
+        mut,
+        seeds=[
+            b"metabata",
+            token_metadata_program.key().as_ref(),
+            collection_mint.key().as_ref()
+            ],
+        bump,
+        seeds::program=token_metadata_program.key())]
+    pub metadata: UncheckedAccount<'info>,
+
+    /// CHECK: This account is validated by the token metadata program during instruction execution.
+    #[account(
+        mut,
+        seeds=[
+            b"metabata",
+            token_metadata_program.key().as_ref(),
+            collection_mint.key().as_ref(),
+            b"edition".as_ref()],
+        bump,
+        seeds::program=token_metadata_program.key())]
+    pub master_edition: UncheckedAccount<'info>,
+
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
 }
--- token-lottery/anchor/programs/tokenlottery/src/lib.rs
+++ token-lottery/anchor/programs/tokenlottery/src/lib.rs
@@ -7,19 +7,27 @@ declare_id!("4VNuGptng1b6vwb8fAMsy5Y5Kg7rKsaZT6bTXL9rrK8U");
 
 #[program]
 pub mod tokenlottery {
-
     use super::*;
-    pub fn intialize_config(ctx:Context<InitializeConfig>,start_time:u64,end_time:u64,ticket_price:u64)->Result<()>{
-      ctx.accounts.token_lottery.bump=ctx.bumps.token_lottery;
-      ctx.accounts.token_lottery.start_time=start_time;
-      ctx.accounts.token_lottery.end_time=end_time;
-      ctx.accounts.token_lottery.ticket_price=ticket_price;
-      ctx.accounts.token_lottery.authority=*ctx.accounts.payer.key;
-      ctx.accounts.token_lottery.lottery_pot_amount=0;
-      ctx.accounts.token_lottery.total_tickets=0;
-      ctx.accounts.token_lottery.randomness_account=Pubkey::default();
-      ctx.accounts.token_lottery.winner_chosen=false;
-      Ok(())
+
+    pub fn intialize_config(
+        ctx: Context<InitializeConfig>,
+        start_time: u64,
+        end_time: u64,
+        ticket_price: u64,
+    ) -> Result<()> {
+        ctx.accounts.token_lottery.bump = ctx.bumps.token_lottery;
+        ctx.accounts.token_lottery.start_time = start_time;
+        ctx.accounts.token_lottery.end_time = end_time;
+        ctx.accounts.token_lottery.ticket_price = ticket_price;
+        ctx.accounts.token_lottery.authority = *ctx.accounts.payer.key;
+        ctx.accounts.token_lottery.lottery_pot_amount = 0;
+        ctx.accounts.token_lottery.total_tickets = 0;
+        ctx.accounts.token_lottery.randomness_account = Pubkey::default();
+        ctx.accounts.token_lottery.winner_chosen = false;
+        Ok(())
     }
 
+    pub fn initialize_lottery(ctx: Context<InitializeLottery>) -> Result<()> {
+        Ok(())
+    }
 }
