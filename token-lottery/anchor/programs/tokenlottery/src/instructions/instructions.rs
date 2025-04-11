4512a3cc7eb592dec4c027d4b657ea229fdf6a9f
luja <lujachitrakar156@gmail.com>
2025-04-11 11:29:53 +0000

    finishing buy tickets

--- token-lottery/anchor/programs/tokenlottery/src/instructions/instructions.rs
+++ token-lottery/anchor/programs/tokenlottery/src/instructions/instructions.rs
@@ -83,15 +83,95 @@ pub struct InitializeLottery<'info> {
 }
 
 #[derive(Accounts)]
-pub struct BuyTicket<'info>{
+pub struct BuyTicket<'info> {
     #[account(mut)]
-    pub payer:Signer<'info>,
+    pub payer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=[b"token_lottery".as_ref()],
+        bump=token_lottery.bump,
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+
+    #[account(
+        mut,
+        seeds=[b"collection_mint".as_ref()],
+        bump
+    )]
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+
+    /// CHECK: This account is validated by the token metadata program during instruction execution.
+    #[account(
+        mut,
+        seeds=[b"metadata",token_metadata_program.key().as_ref(),ticket_mint.key().as_ref()],
+        bump,
+        seeds::program=token_metadata_program.key()
+    )]
+    pub ticket_metadata: UncheckedAccount<'info>,
+
+    /// CHECK: This account is validated by the token metadata program during instruction execution.
+    #[account(
+        mut,
+        seeds=[
+       b"metadata",
+            token_metadata_program.key().as_ref(),
+            ticket_mint.key().as_ref(),
+            b"edition"
+        ],
+        bump,
+        seeds::program=token_metadata_program.key(),
+    )]
+    pub ticket_master_edition: UncheckedAccount<'info>,
+
+    /// CHECK: This account is validated by the token metadata program during instruction execution.
+    #[account(
+        mut,
+        seeds=[b"metadata",token_metadata_program.key().as_ref(),collection_mint.key().as_ref()],
+        bump,
+        seeds::program=token_metadata_program.key()
+    )]
+    pub collection_metadata: UncheckedAccount<'info>,
+
+    /// CHECK: This account is validated by the token metadata program during instruction execution.
+    #[account(
+        mut,
+        seeds=[
+       b"metadata",
+            token_metadata_program.key().as_ref(),
+            collection_mint.key().as_ref(),
+            b"edition"
+        ],
+        bump,
+        seeds::program=token_metadata_program.key(),
+    )]
+    pub collection_master_edition: UncheckedAccount<'info>,
 
     #[account(
         init,
+        payer=payer,
+        mint::decimals=0,
+        mint::authority=collection_mint,
+        mint::freeze_authority=collection_mint,
+        seeds=[token_lottery.total_tickets.to_le_bytes().as_ref()],
+        bump
     )]
+    pub ticket_mint: InterfaceAccount<'info, Mint>,
 
-    pub system_program:Program<'info,System>
+    #[account(
+        init,
+        payer=payer,
+        associated_token::mint=ticket_mint,
+        associated_token::authority=payer,
+        associated_token::token_program=token_program,
+    )]
+    pub destination: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
@@ -108,4 +188,3 @@ pub struct TokenLottery {
     pub randomness_account: Pubkey,
     pub bump: u8,
 }
-
--- token-lottery/anchor/programs/tokenlottery/src/lib.rs
+++ token-lottery/anchor/programs/tokenlottery/src/lib.rs
@@ -1,7 +1,15 @@
 #![allow(clippy::result_large_err)]
 use anchor_lang::prelude::*;
-use anchor_spl::metadata::{create_metadata_accounts_v3, CreateMetadataAccountsV3};
 use anchor_spl::token_interface::{mint_to, MintTo};
+
+use anchor_spl::metadata::{
+    create_master_edition_v3, create_metadata_accounts_v3,
+    mpl_token_metadata::{
+        types::{CollectionDetails, Creator, DataV2},
+    },
+ sign_metadata,SetAndVerifySizedCollectionItem, CreateMasterEditionV3, CreateMetadataAccountsV3,
+    SignMetadata,
+};
 
 pub mod instructions;
 pub use instructions::*;
@@ -18,11 +26,8 @@ pub const URI:&str="https://media.istockphoto.com/id/1500283713/vector/cinema-ti
 #[program]
 pub mod tokenlottery {
 
-    use anchor_spl::metadata::{
-        create_master_edition_v3,
-        mpl_token_metadata::types::{CollectionDetails, Creator, DataV2},
-        sign_metadata, CreateMasterEditionV3, SignMetadata,
-    };
+    use anchor_lang::system_program;
+    use anchor_spl::metadata::set_and_verify_sized_collection_item;
 
     use super::*;
 
@@ -122,9 +127,9 @@ pub mod tokenlottery {
 
         sign_metadata(CpiContext::new_with_signer(
             ctx.accounts.collection_mint.to_account_info(),
-            SignMetadata{
-                creator:ctx.accounts.collection_mint.to_account_info(),
-                metadata:ctx.accounts.metadata.to_account_info(),
+            SignMetadata {
+                creator: ctx.accounts.collection_mint.to_account_info(),
+                metadata: ctx.accounts.metadata.to_account_info(),
             },
             &signer_seeds,
         ))?;
@@ -132,7 +137,127 @@ pub mod tokenlottery {
         Ok(())
     }
 
-    pub fn buy_ticket(ctx:Context<BuyTicket>)->Result<()>{
+    pub fn buy_ticket(ctx: Context<BuyTicket>) -> Result<()> {
+        let clock = Clock::get()?;
+        let ticket_name = NAME.to_owned()
+            + ctx
+                .accounts
+                .token_lottery
+                .total_tickets
+                .to_string()
+                .as_str();
+
+        if clock.slot < ctx.accounts.token_lottery.start_time
+            || clock.slot > ctx.accounts.token_lottery.end_time
+        {
+            return Err(ErrorCode::LotteryNotOpen.into());
+        }
+
+        system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                system_program::Transfer {
+                    from: ctx.accounts.payer.to_account_info(),
+                    to: ctx.accounts.token_lottery.to_account_info(),
+                },
+            ),
+            ctx.accounts.token_lottery.ticket_price,
+        )?;
+
+        let signer_seeds: &[&[&[u8]]] =
+            &[&[b"collection_mint".as_ref(), &[ctx.bumps.collection_mint]]];
+
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    mint: ctx.accounts.ticket_mint.to_account_info(),
+                    to: ctx.accounts.destination.to_account_info(),
+                    authority: ctx.accounts.collection_mint.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            1,
+        )?;
+
+        msg!("Creating metadata acount");
+
+        create_metadata_accounts_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMetadataAccountsV3 {
+                    metadata: ctx.accounts.ticket_metadata.to_account_info(),
+                    mint: ctx.accounts.ticket_mint.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    update_authority: ctx.accounts.collection_mint.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            DataV2 {
+                name: ticket_name,
+                symbol: SYMBOL.to_string(),
+                uri: URI.to_string(),
+                seller_fee_basis_points: 0,
+                creators: None,
+                collection: None,
+                uses: None,
+            },
+            true,
+            true,
+            None,
+        )?;
+
+        msg!("Creating master edition");
+
+        create_master_edition_v3(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                CreateMasterEditionV3 {
+                    payer: ctx.accounts.payer.to_account_info(),
+                    edition: ctx.accounts.ticket_master_edition.to_account_info(),
+                    mint: ctx.accounts.ticket_mint.to_account_info(),
+                    mint_authority: ctx.accounts.collection_mint.to_account_info(),
+                    update_authority: ctx.accounts.collection_mint.to_account_info(),
+                    metadata: ctx.accounts.ticket_metadata.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            Some(0),
+        )?;
+
+        msg!("Verifying  collection");
+
+        set_and_verify_sized_collection_item(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_metadata_program.to_account_info(),
+                SetAndVerifySizedCollectionItem {
+                    metadata: ctx.accounts.ticket_metadata.to_account_info(),
+                    collection_authority: ctx.accounts.collection_mint.to_account_info(),
+                    payer: ctx.accounts.payer.to_account_info(),
+                    update_authority: ctx.accounts.collection_mint.to_account_info(),
+                    collection_master_edition: ctx.accounts.collection_master_edition.to_account_info(),
+                    collection_metadata: ctx.accounts.collection_metadata.to_account_info(),
+                    collection_mint:ctx.accounts.collection_mint.to_account_info(),
+                },
+                &signer_seeds,
+            ),
+            None,
+        )?;
+
+        ctx.accounts.token_lottery.total_tickets+=1;
+
         Ok(())
     }
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("LOttery is not open")]
+    LotteryNotOpen,
 }
